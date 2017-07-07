f39b2e235031c0d4c24aeef01336819814508d8e
freek <freek@spatie.be>
2017-07-07 22:56:35 +0200

    add config

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -1,10 +1,33 @@
 <?php
 
+use Spatie\ImageOptimizer\Optimizers\Gifsicle;
+use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
+use Spatie\ImageOptimizer\Optimizers\Optipng;
+use Spatie\ImageOptimizer\Optimizers\Pngquant;
+use Spatie\ImageOptimizer\Optimizers\Svgo;
+
 return [
     'optimizers' => [
-        \Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
-            '--bla',
-            '--bla2'
+
+        Jpegoptim::class => [
+            '--stip-all',
+            '--all-progressive'
+        ],
+
+        Pngquant::class => [
+            '--force',
+        ],
+        Optipng::class => [
+            '-i0',
+            '-o2',
+            '-quiet',
+        ],
+        Svgo::class => [
+            '--disable=cleanupIDs',
+        ],
+        Gifsicle::class => [
+            '-b',
+            '-O3',
         ],
     ],
 
