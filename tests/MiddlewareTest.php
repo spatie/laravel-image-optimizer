22fb4cd6203fb165b0d86b354912182a36dd623e
freek <freek@spatie.be>
2017-07-12 00:04:24 +0200

    add middleware test

---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spatie\LaravelImageOptimizer\Test;
+
+class MiddlewareTest extends TestCase
+{
+    /** @test */
+    public function it_will_try_to_optimize_all_files_in_a_request()
+    {
+
+    }
+}
