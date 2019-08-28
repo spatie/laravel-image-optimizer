7afe2b677905e57c9ad65decaae4ee75d2f6ff9d
crond <crond@me.com>
2019-08-28 18:03:32 +0300

    add support for webp

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -5,6 +5,7 @@ use Spatie\ImageOptimizer\Optimizers\Optipng;
 use Spatie\ImageOptimizer\Optimizers\Gifsicle;
 use Spatie\ImageOptimizer\Optimizers\Pngquant;
 use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
+use Spatie\ImageOptimizer\Optimizers\Cwebp;
 
 return [
     /*
@@ -36,6 +37,13 @@ return [
         Gifsicle::class => [
             '-b', // required parameter for this package
             '-O3', // this produces the slowest but best results
+        ],
+
+        Cwebp::class => [
+            '-m 6', // for the slowest compression method in order to get the best compression.
+            '-pass 10', // for maximizing the amount of analysis pass.
+            '-mt', // multithreading for some speed improvements.
+            '-q 90' //Quality factor that brings the least noticeable changes.
         ],
     ],
 
