2f3aabdd4ab78b16b29cb03e4897119db65d0a32
crond <crond@me.com>
2019-08-28 18:11:19 +0300

    fix code style

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ return [
             '-m 6', // for the slowest compression method in order to get the best compression.
             '-pass 10', // for maximizing the amount of analysis pass.
             '-mt', // multithreading for some speed improvements.
-            '-q 90' //Quality factor that brings the least noticeable changes.
+            '-q 90', //quality factor that brings the least noticeable changes.
         ],
     ],
 
--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -1,11 +1,12 @@
 <?php
 
 use Spatie\ImageOptimizer\Optimizers\Svgo;
+use Spatie\ImageOptimizer\Optimizers\Cwebp;
 use Spatie\ImageOptimizer\Optimizers\Optipng;
 use Spatie\ImageOptimizer\Optimizers\Gifsicle;
 use Spatie\ImageOptimizer\Optimizers\Pngquant;
 use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
-use Spatie\ImageOptimizer\Optimizers\Cwebp;
+
 
 return [
     /*
@@ -43,7 +44,7 @@ return [
             '-m 6', // for the slowest compression method in order to get the best compression.
             '-pass 10', // for maximizing the amount of analysis pass.
             '-mt', // multithreading for some speed improvements.
-            '-q 90' //Quality factor that brings the least noticeable changes.
+            '-q 90', // quality factor that brings the least noticeable changes.
         ],
     ],
 
