3e1a2d278d7effd7baf9cd511601375e36c10b87
Freek Van der Herten <freekmurze@users.noreply.github.com>
2020-09-08 11:51:36 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,6 +23,7 @@ abstract class TestCase extends OrchestraTestCase
 
     /**
      * @param \Illuminate\Foundation\Application $app
+     *
      * @return array
      */
     protected function getPackageAliases($app)
@@ -39,7 +40,7 @@ abstract class TestCase extends OrchestraTestCase
         $files = scandir($tempDirPath);
 
         foreach ($files as $file) {
-            if (! in_array($file, ['.', '..', '.gitignore'])) {
+            if (!in_array($file, ['.', '..', '.gitignore'])) {
                 unlink("{$tempDirPath}/{$file}");
             }
         }
