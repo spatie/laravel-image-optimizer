4f0f376598aa8f4cf78b05a9c0a010c444053c6b
guanguans <ityaozm@gmail.com>
2025-12-03 19:46:56 +0800

    chore(facade): add PHPDoc annotations for ImageOptimizer facade
    - Added PHPDoc annotations to the ImageOptimizer facade to document available methods.
    - Methods include `getOptimizers`, `addOptimizer`, `setOptimizers`, `setTimeout`, `useLogger`, and `optimize`.
    - Annotations provide better IDE support and improve code readability.
    - Linked the facade to the `Spatie\ImageOptimizer\OptimizerChain` for reference.

--- src/Facades/ImageOptimizer.php
+++ src/Facades/ImageOptimizer.php
@@ -4,6 +4,16 @@ namespace Spatie\LaravelImageOptimizer\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array getOptimizers()
+ * @method static \Spatie\ImageOptimizer\OptimizerChain addOptimizer(\Spatie\ImageOptimizer\Optimizer $optimizer)
+ * @method static \Spatie\ImageOptimizer\OptimizerChain setOptimizers(array $optimizers)
+ * @method static \Spatie\ImageOptimizer\OptimizerChain setTimeout(int $timeoutInSeconds)
+ * @method static \Spatie\ImageOptimizer\OptimizerChain useLogger(\Psr\Log\LoggerInterface $log)
+ * @method static void optimize(string $pathToImage, string|null $pathToOutput = null)
+ *
+ * @see \Spatie\ImageOptimizer\OptimizerChain
+ */
 class ImageOptimizer extends Facade
 {
     protected static function getFacadeAccessor()
